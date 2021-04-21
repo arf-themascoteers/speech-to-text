3e0892fe5b593521bd30c36a4b26d186d64736c0
arif <arif@bitmascot.com>
2021-04-21 12:23:02 +1000

    Hello

---
+++ csv_processor.py
@@ -0,0 +1,21 @@
+import csv
+
+from student import Student
+
+
+class CSVImporter:
+    def __init__(self):
+        pass
+
+    def process(self):
+        students = []
+        with open("data/small.csv", newline='') as csv_file:
+            reader = csv.reader(csv_file, delimiter=',', quotechar='|')
+            for row in reader:
+                students.append(Student(row))
+        return students
+
+
+ci = CSVImporter()
+students = ci.process()
+print(students)
---
+++ student.py
@@ -0,0 +1,14 @@
+class Student:
+    def __init__(self, row):
+        self.name = row[0]
+        self.code = row[1]
+        self.reference = row[2]
+        self.award = row[3]
+        self.type = row[4]
+        
+    def __str__(self):
+        return f"{self.name} {self.code} {self.reference} {self.award} {self.type}"
+
+    def __repr__(self):
+        return "\n" + self.__str__() + "\n"
+
